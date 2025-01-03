6ac528b9e6973a25657faa9bf50a6fb4733b7d32
Tech-with-anmol <anmollklfh@gmail.com>
2025-01-03 23:15:54 +0530

    Onboarding screen addition

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,113 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Dimensions, SafeAreaView, Platform } from "react-native";
+import Onboarding from 'react-native-onboarding-swiper'
+import LottieView from "lottie-react-native";
+import { Stack } from "expo-router";
+import { useFonts, Poppins_500Medium,Poppins_400Regular } from "@expo-google-fonts/poppins";
+import { useEffect } from "react";
+import * as SplashsScreen from 'expo-splash-screen'
+
+const {height, width} = Dimensions.get('window');
+
+SplashsScreen.preventAutoHideAsync();
 
 export default function Index() {
+
+  const [loaded, error] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+  });
+
+  useEffect(() => {
+    if(loaded || error) {
+      SplashsScreen.hideAsync();
+    }
+  }, [loaded, error]);
+  
+  if(!loaded && !error) {
+    return null;
+  }
+
+  
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    
+    <View style={styles.container}>
+      <Stack.Screen options={{headerShown: false}}/>
+      <Onboarding
+       titleStyles={{
+        fontFamily: Platform.select({
+          android: 'Poppins_500Medium',
+          ios: 'Poppins-Medium'
+        })
+       }}
+       subTitleStyles={{
+        textAlign: 'center',
+        padding: 10,
+        fontFamily: Platform.select({
+          android: 'Poppins_500Medium',
+          ios: 'Poppins-Medium'
+        })
+       }}
+       containerStyles={{paddingHorizontal: 20}}
+       pages={[
+        {
+          backgroundColor: 'rgba(221, 214, 111, 0.8)',
+          image: (
+            <View>
+              <LottieView
+               autoPlay
+               loop
+               style={{
+                 height: height*0.5,
+                 width: width
+                 
+               }}
+               source={require('../assets/animations/anione.json')} />
+            </View>
+          ),
+          title: 'OnlyBooks',
+          subtitle: 'A place to discuss books, quotes,  education & research',
+        },
+        {
+          backgroundColor: 'rgba(185, 226, 131, 0.9)',
+          image: (
+            <View >
+               <LottieView 
+               style={{
+                height: height*0.5,
+                width: width
+              }}
+               source={require('../assets/animations/discover.json')} autoPlay loop/>
+            </View>
+          ),
+          title: 'Discover',
+          subtitle: 'Discover awesome quotes, book content, reviews & a whole lot more',
+        },
+        {
+          backgroundColor: 'rgba(238, 199, 155, 0.8)',
+          image: (
+            <View >
+               <LottieView 
+               style={{
+                height: height*0.5,
+                width: width * 0.9
+              }}
+               source={require('../assets/animations/library.json')} autoPlay loop/>
+            </View>
+          ),
+          title: 'Library',
+          subtitle: 'Get collection & AI suggestions',
+        },
+       ]}
+      />
     </View>
+    
   );
 }
+
+const styles = StyleSheet.create({
+  container : {
+    flex: 1
+  },
+  
+})
